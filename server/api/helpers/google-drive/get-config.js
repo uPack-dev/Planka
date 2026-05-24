669348c7c213b977e3c359e5e784743ce0af7258
Niсkolay Lozov <trawe1234@gmail.com>
2026-05-25 00:04:42 +0300

    fix: remove unsafe Google Drive config secret fallback

--- server/api/helpers/google-drive/get-config.js
+++ server/api/helpers/google-drive/get-config.js
@@ -3,8 +3,6 @@
  * Licensed under the Fair Use License: https://github.com/plankanban/planka/blob/master/LICENSE.md
  */
 
-const crypto = require('crypto');
-
 module.exports = {
   sync: false,
 
@@ -13,34 +11,20 @@ module.exports = {
   async fn() {
     const config = await Config.qm.getOneMain();
 
-    const enabled =
-      config.googleDriveEnabled !== undefined
-        ? config.googleDriveEnabled
-        : process.env.GOOGLE_DRIVE_INTEGRATION_ENABLED === 'true';
-    const clientId = config.googleDriveClientId || process.env.GOOGLE_DRIVE_CLIENT_ID;
-    const pickerApiKey = config.googleDrivePickerApiKey || process.env.GOOGLE_DRIVE_PICKER_API_KEY;
-    const pickerAppId = config.googleDrivePickerAppId || process.env.GOOGLE_DRIVE_PICKER_APP_ID;
-    const scopes =
-      config.googleDriveScopes ||
-      process.env.GOOGLE_DRIVE_SCOPES ||
-      'https://www.googleapis.com/auth/drive.file';
+    const enabled = config.googleDriveEnabled !== undefined ? config.googleDriveEnabled : false;
+    const clientId = config.googleDriveClientId;
+    const pickerApiKey = config.googleDrivePickerApiKey;
+    const pickerAppId = config.googleDrivePickerAppId;
+    const scopes = config.googleDriveScopes || 'https://www.googleapis.com/auth/drive.file';
 
     let clientSecret = null;
     if (config.googleDriveClientSecretEncrypted) {
       try {
-        const secret = sails.config.session.secret || process.env.SECRET_KEY || 'default-secret';
-        const key =
-          process.env.GOOGLE_DRIVE_TOKEN_ENCRYPTION_KEY &&
-          process.env.GOOGLE_DRIVE_TOKEN_ENCRYPTION_KEY.length === 64
-            ? process.env.GOOGLE_DRIVE_TOKEN_ENCRYPTION_KEY
-            : crypto.createHash('sha256').update(secret).digest('hex');
-
+        const key = sails.helpers.googleDrive.getEncryptionKey();
         clientSecret = sails.helpers.utils.decrypt(config.googleDriveClientSecretEncrypted, key);
       } catch (err) {
         sails.log.error('Failed to decrypt Google Drive Client Secret:', err);
       }
-    } else if (process.env.GOOGLE_DRIVE_CLIENT_SECRET) {
-      clientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET;
     }
 
     return {
